d73a99cec49e054a9b1a907388f7f8b4fa798c26
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-03-14 17:45:24 -0400

    JS02-Added prompts/confirms with user choices

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*()_+";
+
+// Alert that start the app:
+alert("Let's get started! Please select from the following:")
+
+// Code that prompts for the password length:
+var lengthChoice = +prompt("How many characters? Please choose between 8 and 128:");
+
+while (lengthChoice < 8 || lengthChoice > 128) {
+  var lengthChoice = prompt("Please choose between 8 and 128:");
+}
+
+// Confirm statements that collect info about the type of characters the user wants:
+var lowerChoice = confirm("Include lowercase characters? Click OK for Yes or CANCEL for NO:");
+var upperChoice = confirm("Include UPPERCASE characters? Click OK for YES or CANCEL for NO:");
+var numChoice = confirm("Include numbers? Click OK for YES or CANCEL for NO:");
+var symbolChoice = confirm("Include special characters? Click OK for YES or CANCEL for NO:");
+var charList = "";
